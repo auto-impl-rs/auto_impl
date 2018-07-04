f05169ac22f054b47d21ac344c75e24fdd7413eb
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-07-04 17:00:17 +0200

    Add another example to `simple`

--- examples/simple.rs
+++ examples/simple.rs
@@ -10,6 +10,19 @@ trait Foo<'a, X, Y = i32> {
     // fn foo(&self);
 }
 
+#[auto_impl(Box)]
+trait MyTrait<'a, T>
+    where T: AsRef<str>
+{
+    // type Type1;
+    // type Type2;
+
+    // fn execute1<'b>(&'a self, arg1: &'b T) -> Result<Self::Type1, String>;
+    // fn execute2(&self) -> Self::Type2;
+    // fn execute3(self) -> Self::Type1;
+    // fn execute4() -> &'static str;
+}
+
 
 
 fn main() {}
