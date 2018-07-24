3c9457cfc70e6ee898c872adf6f461dec390e94e
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-07-24 23:52:46 +0200

    Add two examples that fail to compile (to showcase error messages)
    The examples are a bit awkward for some reason: examples are compiled when
    one executes `cargo test`. This is nice, but we actually want those examples
    to fail. We can't just exclude them, so we only compile the interesting
    part when the `fail` cfg is set.
    
    I hope to find a better solution for this...

---
+++ examples/fail_invalid_proxy.rs
@@ -0,0 +1,32 @@
+//! Shows the error message for the case that `#[auto_impl]` was used with
+//! incorrect proxy types. Only proxy types like `&` and `Box` are allowed.
+//!
+//! To build the example, run:
+//!
+//! ```
+//! $ cargo rustc --example fail_invalid_proxy -- --cfg fail
+//! ```
+//!
+//! To build it in nightly mode with better error message, run:
+//!
+//! ```
+//! $ cargo rustc --example fail_invalid_proxy --features nightly -- --cfg fail
+//! ```
+
+#![feature(use_extern_macros)]
+
+
+#[cfg(fail)]
+mod fail {
+    extern crate auto_impl;
+    use self::auto_impl::auto_impl;
+
+
+    #[auto_impl(Boxxi)]
+    trait Foo {
+        fn foo(&self) -> u32;
+    }
+}
+
+
+fn main() {}
---
+++ examples/fail_on_struct.rs
@@ -0,0 +1,32 @@
+//! Shows the error message for the case the `#[auto_impl]` wasn't applied to
+//! a valid trait (in this case a struct).
+//!
+//! To build the example, run:
+//!
+//! ```
+//! $ cargo rustc --example fail_on_struct -- --cfg fail
+//! ```
+//!
+//! To build it in nightly mode with better error message, run:
+//!
+//! ```
+//! $ cargo rustc --example fail_on_struct --features nightly -- --cfg fail
+//! ```
+
+#![feature(use_extern_macros)]
+
+
+#[cfg(fail)]
+mod fail {
+    extern crate auto_impl;
+    use self::auto_impl::auto_impl;
+
+
+    #[auto_impl(&, Box)]
+    struct Foo {
+        x: u32,
+    }
+}
+
+
+fn main() {}
