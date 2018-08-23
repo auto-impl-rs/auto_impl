71ed0d89a2552bf73fb2999550c346c9c8aaf0b5
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-08-23 17:29:05 +0200

    Add tests for `keep_default_for` attribute

---
+++ tests/compile-fail/keep_default_for_on_assoc_type.rs
@@ -0,0 +1,8 @@
+use auto_impl::auto_impl;
+
+
+#[auto_impl(&)]
+trait Foo {
+    #[auto_impl(keep_default_for(&))]
+    type Foo;
+}
---
+++ tests/compile-pass/keep_default_for_with_where_bounds.rs
@@ -0,0 +1,10 @@
+use auto_impl::auto_impl;
+
+
+#[auto_impl(&)]
+trait Foo {
+    fn required(&self);
+
+    #[auto_impl(keep_default_for(&))]
+    fn provided(&self) where Self: Clone {}
+}
