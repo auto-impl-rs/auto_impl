5af2b910c7b86bc4097fc9c32632ab0f982be2d2
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2018-08-23 17:13:31 +0200

    Check `keep_default_for` attribute on methods

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,10 @@ extern crate quote;
 
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
-use syn::{
-    TraitItemMethod,
-    visit_mut::{VisitMut, visit_item_trait_mut},
-};
+use quote::ToTokens;
 
 mod analyze;
+mod attr;
 mod diag;
 mod gen;
 mod proxy;
@@ -29,69 +27,58 @@ use crate::{
 /// See crate documentation for more information.
 #[proc_macro_attribute]
 pub fn auto_impl(args: TokenStream, input: TokenStream) -> TokenStream {
-    // We use the closure trick to catch errors until the `catch` syntax is
-    // available. If an error occurs, we won't modify or add any tokens.
-    let result = || -> Result<TokenStream, ()> {
-        // Try to parse the token stream from the attribute to get a list of
-        // proxy types.
-        let proxy_types = proxy::parse_types(args)?;
+    // Try to parse the token stream from the attribute to get a list of proxy
+    // types.
+    let proxy_types = proxy::parse_types(args);
 
-        // Try to parse the item the `#[auto_impl]` attribute was applied to as
-        // trait. Unfortunately, `parse()` consume the token stream, so we have
-        // to clone it.
-        match syn::parse::<syn::ItemTrait>(input.clone()) {
-            // The attribute was applied to a valid trait. Now it's time to
-            // execute the main step: generate a token stream which contains an
-            // impl of the trait for each proxy type.
-            Ok(mut trait_def) => {
-                let generated = gen::gen_impls(&proxy_types, &trait_def)?;
+    // Try to parse the item the `#[auto_impl]` attribute was applied to as
+    // trait. Unfortunately, `parse()` consume the token stream, so we have to
+    // clone it.
+    match syn::parse::<syn::ItemTrait>(input.clone()) {
+        // The attribute was applied to a valid trait. Now it's time to execute
+        // the main step: generate a token stream which contains an impl of the
+        // trait for each proxy type.
+        Ok(mut trait_def) => {
+            let generated = proxy_types.and_then(|proxy_types| {
+                gen::gen_impls(&proxy_types, &trait_def)
+            });
 
-                // Before returning the trait definition, we have to remove all
-                // `#[auto_impl(...)]` attributes on any methods.
-                remove_our_attrs(&mut trait_def);
+            // Before returning the trait definition, we have to remove all
+            // `#[auto_impl(...)]` attributes on all methods.
+            attr::remove_our_attrs(&mut trait_def);
 
-                Ok(quote! { #trait_def #generated }.into())
-            },
-
-            // If the token stream could not be parsed as trait, this most
-            // likely means that the attribute was applied to a non-trait item.
-            // Even if the trait definition was syntactically incorrect, the
-            // compiler usually does some kind of error recovery to proceed. We
-            // get the recovered tokens.
-            Err(e) => {
-                // We have to take the detour through TokenStream2 to get a
-                // good span for the error.
-                TokenStream2::from(input.clone()).span()
-                    .err("couldn't parse trait item")
-                    .note(e.to_string())
-                    .note("the #[auto_impl] attribute can only be applied to traits!")
-                    .emit();
-
-                Err(())
+            match generated {
+                // No errors at all => output the trait and our generated impls
+                Ok(generated) => quote! { #trait_def #generated }.into(),
+                Err(_) => {
+                    // We combine the token stream of the modified trait
+                    // definition with the generated errors (which are tokens
+                    // on stable due to the `compile_error!` hack).
+                    vec![
+                        TokenStream::from(trait_def.into_token_stream()),
+                        diag::error_tokens()
+                    ].into_iter().collect()
+                }
             }
-        }
-    }();
+        },
 
-    // If everything went well, we just return the new token stream. If an
-    // error occured, we combine the original token stream with the generated
-    // errors (which are tokens on stable due to the `compile_error!` hack).
-    match result {
-        Ok(tokens) => tokens,
-        Err(_) => vec![input, diag::error_tokens()].into_iter().collect(),
-    }
-}
+        // If the token stream could not be parsed as trait, this most
+        // likely means that the attribute was applied to a non-trait item.
+        // Even if the trait definition was syntactically incorrect, the
+        // compiler usually does some kind of error recovery to proceed. We
+        // get the recovered tokens.
+        Err(e) => {
+            // We have to take the detour through TokenStream2 to get a
+            // good span for the error.
+            TokenStream2::from(input.clone()).span()
+                .err("couldn't parse trait item")
+                .note(e.to_string())
+                .note("the #[auto_impl] attribute can only be applied to traits!")
+                .emit();
 
-/// Removes all `#[auto_impl]` attributes that are attached to methods of the
-/// given trait.
-fn remove_our_attrs(trait_def: &mut syn::ItemTrait) {
-    struct AttrRemover;
-    impl VisitMut for AttrRemover {
-        fn visit_trait_item_method_mut(&mut self, m: &mut TraitItemMethod) {
-            m.attrs.retain(|a| !a.path.segments.iter().all(|seg| {
-                seg.ident == "auto_impl" && seg.arguments.is_empty()
-            }));
+            // We combine the original token stream with the generated errors
+            // (which are tokens on stable due to the `compile_error!` hack).
+            vec![input, diag::error_tokens()].into_iter().collect()
         }
     }
-
-    visit_item_trait_mut(&mut AttrRemover, trait_def);
 }
