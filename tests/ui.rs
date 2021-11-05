c686f3d681af3cb3039ae8127a51a691046382ae
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-11-05 17:18:18 +0100

    Split UI tests into two test functions and change logic in CI script
    The problem is that we only want to run compile-fail tests for a single
    compiler version (the current stable) on CI. This is because compiler
    errors change over time and we can only support one version at a time.
    
    Before this commit, this was solved by simply disabling all tests for
    non-stable compilers. But this had a few downsides:
    
    - You had no way of running these tests with a nightly compiler locally,
      which is a valid thing to do.
    - We also disabled the compile-pass tests which don't have the problem
      and should always work on all compilers.
    - I want to add `1.37` (MSRV) to our CI matrix. There, the "stable"
      logic would break, as that's stable, too.
    
    So now the only check is in the CI script itself.

--- tests/ui.rs
+++ tests/ui.rs
@@ -1,9 +1,13 @@
-#[rustversion::stable]
-#[test]
-fn ui() {
-    use trybuild::TestCases;
+use trybuild::TestCases;
 
+#[test]
+fn ui_compile_pass() {
+    let t = TestCases::new();
+    t.pass("tests/compile-pass/*.rs");
+}
+
+#[test]
+fn ui_compile_fail() {
     let t = TestCases::new();
     t.compile_fail("tests/compile-fail/*.rs");
-    t.pass("tests/compile-pass/*.rs");
 }
