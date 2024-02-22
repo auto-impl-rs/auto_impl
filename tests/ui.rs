e96da349e43a72242ded9e1027fb804ce54bc24e
makcandrov <makcandrov@proton.me>
2024-02-21 18:40:13 -0800

    feat: support async traits

--- tests/ui.rs
+++ tests/ui.rs
@@ -15,7 +15,14 @@ fn ui_compile_fail() {
 
 #[rustversion::since(1.51)]
 #[test]
-fn ui_recent_compile_pass() {
+fn ui_since_1_51_compile_pass() {
     let t = TestCases::new();
-    t.pass("tests/recent/compile-pass/*.rs");
+    t.pass("tests/since_1.51/compile-pass/*.rs");
+}
+
+#[rustversion::since(1.75)]
+#[test]
+fn ui_since_1_75_compile_pass() {
+    let t = TestCases::new();
+    t.pass("tests/since_1.75/compile-pass/*.rs");
 }
